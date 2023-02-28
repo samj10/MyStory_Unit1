0f0d01b0c794bb10c8deb1c6bb1cca3552acb6a1
samj10 <sjain23@student.gsu.edu>
2023-02-28 14:53:24 -0500

    Initial Commit

---
+++ MyStory_Unit1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStory_Unit1
+//
+//  Created by Samyak Jain on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
